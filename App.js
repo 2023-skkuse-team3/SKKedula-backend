1292bf56f85b40dfe4f68d52aa2f9bfaceb6c7ce
frica12 <fricaa@naver.com>
2023-10-25 03:37:48 +0900

    Logout API

--- App.js
+++ App.js
@@ -5,36 +5,55 @@ const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 const app = express();
 const db = new sqlite3.Database("./skkedula.db");
 app.use(bodyParser.json());
 
+app.use(
+  session({
+    secret: "secret_key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 const saltRounds = 10;
 
 // 회원가입
 app.post("/register", (req, res) => {
-  const { student_ID, name, id, password, confirmPassword } = req.body;
+  const { id, name, password, confirmPassword } = req.body;
 
   if (password !== confirmPassword) {
     return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
   }
 
-  bcrypt.hash(password, saltRounds, function (err, hash) {
+  db.get("SELECT ID FROM Students WHERE ID = ?", [id], (err, row) => {
     if (err) {
-      return res.status(500).json({ message: "비밀번호 해싱 오류" });
+      return res.status(500).json({ message: "DB 조회 오류" });
     }
 
-    db.run(
-      "INSERT INTO Students (Student_ID, Name, ID, PW) VALUES (?, ?, ?, ?)",
-      [student_ID, name, id, hash],
-      function (err) {
-        if (err) {
-          return res.status(500).json({ message: "유저 등록 오류" });
-        }
-        res.json({ message: "회원가입이 정상적으로 완료되었습니다." });
+    if (row) {
+      return res.status(400).json({ message: "이미 존재하는 아이디입니다." });
+    }
+
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      if (err) {
+        return res.status(500).json({ message: "비밀번호 해싱 오류" });
       }
-    );
+
+      db.run(
+        "INSERT INTO Students (ID, Name, PW) VALUES (?, ?, ?)",
+        [id, name, hash],
+        function (err) {
+          if (err) {
+            return res.status(500).json({ message: "유저 등록 오류" });
+          }
+          res.json({ message: "회원가입이 정상적으로 완료되었습니다." });
+        }
+      );
+    });
   });
 });
 
@@ -53,18 +72,31 @@ app.post("/login", (req, res) => {
 
     bcrypt.compare(password, row.PW, function (err, result) {
       if (err) {
-        return res
-          .status(500)
-          .json({ message: "비밀번호가 일치하지 않습니다." });
+        return res.status(500).json({ message: "비밀번호 비교 오류" });
       }
 
       if (result) {
+        req.session.userId = id;
         res.json({ message: "로그인 성공" });
       } else {
         res.status(401).json({ message: "로그인 실패" });
       }
     });
   });
+});
+
+// 로그아웃
+app.get("/logout", (req, res) => {
+  if (req.session && req.session.userId) {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).json({ message: "로그아웃 실패" });
+      }
+      res.json({ message: "로그아웃 성공" });
+    });
+  } else {
+    res.status(400).json({ message: "로그인 상태가 아닙니다." });
+  }
 });
 
 const PORT = 3000;
