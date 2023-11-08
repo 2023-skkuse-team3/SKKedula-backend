ac9195ccc53c082466b3a644cefbba909d891857
pegi-os <tlarlguswh@gmail.com>
2023-11-08 21:59:32 +0900

    fixed timetable api for add, delete, search

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
-import SocketIO from "socket.io";
+
 
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
@@ -14,25 +14,9 @@ app.use(bodyParser.json());
 
 const saltRounds = 10;
 
-const database = [
-  {
-    'Course Name': '심층신경망개론\nIntroduction to Deep Neural Networks',
-    'Professor': '이지형',
-    'Class Time': '월10:30-11:45&#8203;``【oaicite:1】``&#8203;,수09:00-10:15&#8203;``【oaicite:0】``&#8203;',
-    'Class Room': '26223.0'
-  },
-];
 
-// const createTableQuery = `
-//   CREATE TABLE IF NOT EXISTS timetable (
-//     enrollment_number INTEGER PRIMARY KEY AUTOINCREMENT,
-//     course_name TEXT,
-//     professor TEXT,
-//     class_time TEXT,
-//     class_room TEXT
-//   )
-// `;
-
+year = 2023;
+semester = 2;
 
 // 회원가입
 app.post("/register", (req, res) => {
@@ -91,30 +75,6 @@ app.post("/login", (req, res) => {
 
 // =======================================================================================================
 
-//과목 삭제, (front에서 요청이 옴 -> db에서 삭제)
-app.delete("/timetable/delete", (req, res) => {
-  const {course_name, professor, class_time, class_room } = req.body; // 
-
-  // DELETE 쿼리를 실행하여 데이터베이스에서 해당 과목 삭제
-  const deleteQuery = `
-    DELETE FROM timetable
-    WHERE course_name = ? 
-    AND professor = ? 
-    AND class_time = ? 
-    AND class_room = ?`;
-
-  db.run(deleteQuery, [course_name, professor, class_time, class_room], function (err) {
-    if (err) {
-      return res.status(500).json({ message: "과목 삭제 실패" });
-    }
-
-    if (this.changes > 0) {
-      res.json({ message: "과목이 삭제되었습니다." }); //db에 변경이 있을 경우 this.changes가 0보다 커진다. 따라서 삭제 된 걸 확인 할 수 있다.
-    } else {
-      res.status(404).json({ message: "과목을 찾을 수 없습니다." });
-    }
-  });
-});
 
 
 //과목 추가, (front에서 data 입력 -> db에 추가) 
@@ -146,22 +106,22 @@ app.post("/timetable/add", (req, res) => {
       });
     } else {
       // 단일 과목인 경우
-      const { course_name, professor, class_time, class_room } = selectedCourse;
-
       // 찾은 과목을 timetable 테이블에 추가합니다.
+      const course_id = courseData[0].course_ID;
+      const student_ID = 10; //일단 임의값
       const insertTimetableQuery = `
-        INSERT INTO timetable (course_name, professor, class_time, class_room)
-        VALUES (?, ?, ?, ?)
+        INSERT INTO Enrollments (student_ID , course_ID)
+        VALUES (?, ?)
       `;
 
-      db.run(insertTimetableQuery, [course_name, professor, class_time, class_room], (err) => {
+      db.run(insertTimetableQuery, [student_ID, course_id], (err) => {
         if (err) {
           return res.status(500).json({ message: "과목 추가 실패" });
         }
 
         res.json({
           message: "과목이 성공적으로 추가되었습니다.",
-          courseData: selectedCourse
+          courseData: courseData
         });
       });
     }
@@ -173,20 +133,17 @@ app.post("/timetable/add", (req, res) => {
 //custom data를 받을 경우에는 유저가 직접 course_name, class_time, class_room 같은 내용을 직접 입력하는 란이 있다면.
 app.post("/timetable/addSelectedCourse", (req, res) => {
   const {
-    course_name,
-    professor,
-    class_time,
-    class_room
+    course_ID
   } = req.body;
-
+  const student_ID = 10; //일단 임의값
   const insertQuery = `
-    INSERT INTO timetable (course_name, professor, class_time, class_room)
-    VALUES (?, ?, ?, ?)
+    INSERT INTO Enrollments (student_ID , course_ID)
+    VALUES (?, ?)
   `;
 
   db.run(
     insertQuery,
-    [course_name, professor, class_time, class_room],
+    [student_ID, course_ID],
     function (err) {
       if (err) {
         return res.status(500).json({ message: "과목 추가 실패" });
@@ -198,82 +155,115 @@ app.post("/timetable/addSelectedCourse", (req, res) => {
 });
 
 
-app.patch("/timetable/edit", (req, res) => {
-  const updatedData = req.body;
-  const courseName = updatedData.course_name;
-  const classTime = updatedData.class_time;
+//과목 삭제, (front에서 요청이 옴 -> db에서 삭제)
+app.delete("/timetable/delete", (req, res) => {
+  const {course_ID } = req.body; // 
+  const student_ID = 10; //일단 임의값
+  // DELETE 쿼리를 실행하여 데이터베이스에서 해당 과목 삭제
+  const deleteQuery = `
+    DELETE FROM Enrollments
+    WHERE student_ID = ?
+    AND course_ID = ? `;
 
-  const updateQuery = `
-    UPDATE timetable
-    SET professor = ?,
-        class_room = ?
-    WHERE course_name = ? AND class_time = ?;
-  `;
-
-  db.run(
-    updateQuery,
-    [updatedData.professor, updatedData.class_room, courseName, classTime],
-    function (err) {
-      if (err) {
-        return res.status(500).json({ message: "과목 업데이트 실패" });
-      }
-
-      res.json({ message: "과목이 성공적으로 업데이트되었습니다." });
+  db.run(deleteQuery, [student_ID, course_ID], function (err) {
+    if (err) {
+      return res.status(500).json({ message: "과목 삭제 실패" });
     }
-  );
+
+    if (this.changes > 0) {
+      res.json({ message: "과목이 삭제되었습니다." }); //db에 변경이 있을 경우 this.changes가 0보다 커진다. 따라서 삭제 된 걸 확인 할 수 있다.
+    } else {
+      res.status(404).json({ message: "과목을 찾을 수 없습니다." });
+    }
+  });
 });
 
 
-//출발지 도착지 클릭시 과목별 정보 조회 -> 클릭하면 backend에는 어떤 data 값?
-//일단 출발지만 만들어봄. 고민.
-app.get("/timetable/infoDeparture", (req, res) => {
-  const departure = req.query; 
+// edit에 어떤것? 어차피 course_id가 바뀌는게 아니라면 db를 건들필요없는데 professor나 이름 같은거 바꾸는거는 db 바꿀 게 없으니 edit api
+// app.patch("/timetable/edit", (req, res) => {
+//   const updatedData = req.body;
+//   const courseName = updatedData.course_name;
+//   const classTime = updatedData.class_time;
+
+//   const updateQuery = `
+//     UPDATE timetable
+//     SET professor = ?,
+//         class_room = ?
+//     WHERE course_name = ? AND class_time = ?;
+//   `;
+
+//   db.run(
+//     updateQuery,
+//     [updatedData.professor, updatedData.class_room, courseName, classTime],
+//     function (err) {
+//       if (err) {
+//         return res.status(500).json({ message: "과목 업데이트 실패" });
+//       }
+
+//       res.json({ message: "과목이 성공적으로 업데이트되었습니다." });
+//     }
+//   );
+// });
 
 
-  const selectQuery = `
-    SELECT *
-    FROM timetable
-    WHERE ( ? )
-  `;
-  //여긴 어디서 data 가져와야는지.
-  db.all( //결과 반환
-    selectQuery,
-    departure,
-    (err, data) => {
-      if (err) {
-        return res.status(500).json({ message: "과목 정보 조회 실패" });
-      }
 
-      res.json(data); //도착지 정보 front에 반환
-    }
-  );
-});
 
-//과목 조회 -> 교수명으로 조회 기능 추가?
 app.get("/timetable/search", (req, res) => {
+  // Extract the search query from the request's query string parameter 'searchQuery'
+ 
   const searchQuery = req.query.searchQuery; 
-  //http://example.com/timetable/search?searchQuery=과목명 만약에 클라이언트에서 이런식으로 data가 들어오면 이런 방법을 쓴다.
 
+  // Log the search query to the console for debugging purposes
+  console.log("Received search query: ", searchQuery);
+  // Define the SQL SELECT query, using a placeholder for parameter substitution
   const selectQuery = `
     SELECT *
     FROM courses  
-    WHERE course_name LIKE ?;
+    WHERE course_name LIKE ?;  // Using the LIKE operator to search for matches in 'course_name'
   `;
-  //여긴 From courses
-  const searchPattern = `%${searchQuery}%`; // LIKE 연산자를 활용하기 위해서 사용.
 
+  // Construct the search pattern with '%' wildcards for partial matches
+  const searchPattern = `%${searchQuery}%`; 
+
+  // Execute the database query with the search pattern
   db.all(
     selectQuery,
-    [searchPattern],
+    [searchPattern],  // Substitute the placeholder with the search pattern
     (err, data) => {
       if (err) {
-        return res.status(500).json({ message: "과목 검색 실패" });
+        // Send a server error response if there's an issue executing the query
+        return res.status(500).json({ message: "과목 검색 실패", error: err.message });
       }
 
-      res.json(data); //도착지 정보 front에 반환
+      // Send the search results back to the client
+      res.json(data);
     }
   );
 });
+
+
+app.post("/timetable/test", (req, res) => {
+  // Extract course details from request body
+  const { course_id, course_name, professor, time_start, class_room } = req.body;
+
+  // Prepare INSERT statement
+  const insertQuery = `
+    INSERT INTO courses (Course_ID, Course_name, Professor, Time_start, Room_num)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  // Execute INSERT statement
+  db.run(insertQuery, [course_id, course_name, professor, class_time, class_room], function(err) {
+    if (err) {
+      // If there is an error, send back an error response
+      res.status(500).json({ message: "Failed to insert course into database", error: err.message });
+    } else {
+      // If successful, send back a success response
+      res.status(200).json({ message: "Course added successfully"});
+    }
+  });
+});
+
 
 
 const PORT = 3000;
